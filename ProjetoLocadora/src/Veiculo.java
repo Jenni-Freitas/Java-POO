6e51ae05967a7a940a2fab82411d209201cd8f12
Jenniffer <jennigeanne16@gmail.com>
2026-04-09 21:50:44 -0300

    feat: trabalhando com a montadora de veiculos e trabalhando com POO

---
+++ ProjetoLocadora/src/Veiculo.java
@@ -0,0 +1,86 @@
+//SISTEMA DE VEICULOS - 4 pilares da POO em Java
+// Uma locadora de veiculos precisa calcular o valor da diaria
+// De diferentes tipos de veiculos (Carros e Moto)
+//Cada tipo tem seu proprio calculo
+
+
+//PILAR 1 - ABSTRAÇÃO
+//MODELAR APENAS O QUE IMPORTA: marca, modelo, ano, placa, preço base;
+//METODO - CalcularDiaria()
+//Abstrato pois cada veiculo terá sua própria diária (calculo).
+
+abstract class Veiculo {
+
+    //ATRIBUTOS PROTEGIDOS (ENCAPSULAMENTO)
+    private String marca;
+    private String modelo;
+    private int ano;
+    private String placa;
+    private double precoBase;
+
+    public Veiculo(String marca, String modelo, int ano, String placa, double precoBase) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.placa = placa;
+        this.precoBase = precoBase;
+    }
+
+    //Metodo Abstrato - CADA SUBTIPO DEFINE O CALCULO
+    public abstract double calcularDiaria();
+
+    //PILAR 2 - ENCAPSULAMENTO
+    // MODIFICADORES VIA GETTERS E SETTERS
+    //O SETTER DE PRECOBASE TEM VALIDAÇÃO: NÃO ACEITA VALORES NEGATIVOS
+    //EXEMPLO GETTERS
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public double getPrecoBase() {
+        return precoBase;
+    }
+
+    //EXEMPLO SETTERS
+    public void setPrecoBase(double precoBase) {
+        if (precoBase < 0) {
+            System.out.println("Erro: Preço não pode ser negativo!");
+            return;
+        }
+        this.precoBase = precoBase;
+    }
+
+    public String toString() {
+        return marca + " " + modelo + " (" + ano + ")";
+    }
+
+
+//PILAR 3 - HERANÇA
+//Carro e Moto herdam tudo de veiculo, mas cada um adiciona
+//Seus próprios atributos e implementa calcularDiaria() do seu jeito
+
+//CARRO - Qtd Portas (atributos)
+//Metodo calcularDiaria - Se qtdePortas >= 4 então acréscimo de 20%
+
+    abstract class Carro extends Veiculo {
+        private int quantidadePortas;
+
+        public Carro(String marca, String modelo, int ano, String placa, double precoBase, int quantidadePortas) {
+            super(marca, modelo, ano, placa, precoBase);
+            this.quantidadePortas = quantidadePortas;
+        }
+
+    }
+}
