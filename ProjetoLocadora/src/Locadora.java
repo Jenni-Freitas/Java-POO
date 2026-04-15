a97f5fa060228a5e8556bcc0a0945a48f11c7bdd
Jenniffer <jennigeanne16@gmail.com>
2026-04-14 21:40:05 -0300

    feat: Finalizando o projeto de locadora de veiculos aplicando os fundamentos de POO

--- ProjetoLocadora/src/Locadora.java
+++ ProjetoLocadora/src/Locadora.java
@@ -8,7 +8,7 @@ public class Locadora {
         this.frota = new ArrayList<>();
     }
 
-    public void adicionarVeículo(Veiculo veiculo){
+    public void adicionarVeiculo(Veiculo veiculo){
         frota.add(veiculo);
     }
     public void listarVeiculo(){
@@ -16,10 +16,26 @@ public class Locadora {
             Veiculo veiculo = frota.get(i);
         System.out.printf("[%d] %s %s | Diária: R$ %.2f\n", (i + 1), veiculo.getMarca(), veiculo.getModelo(), veiculo.calcularDiaria());
         }
+
+        System.out.println("============");
+    }
+    public List<Veiculo> getFrota(){
+        return frota;
     }
 
+    public void aluguelVeiculo (int dias, int idVeiculo){
+        //Implementar a lógica para alugar um carro/moto.
+      Veiculo veiculo = frota.get(idVeiculo - 1);
 
-}
+      double total = veiculo.calcularDiaria() * dias;
+
+        System.out.println("=== Resumo do aluguel veiculo: ===");
+        System.out.println("Veiculo" + veiculo.exibirDados());
+        System.out.println("Quantidade de dias: " + dias);
+        System.out.printf("Valor diário: %.2f", veiculo.calcularDiaria());
+        System.out.printf("\nTotal do período da regência: %.2f ", total);
+        }
+    }
 
 //Cadastrar Veículo
 //Listar Veiculo
--- ProjetoLocadora/src/Main.java
+++ ProjetoLocadora/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     static void main(String [] args) {
 
@@ -7,11 +9,28 @@ public class Main {
         Moto moto1 = new Moto("Suzuki","Hayabusa",2005,"xxx-9999",190,1000);
 //        System.out.println(moto1.exibirDados());
 
+        Carro carro2 = new Carro("Volkswagen ", "Voyage", 2012, "jjj-9999", 150, 4);
+        System.out.println(carro2.exibirDados());
+
+        Moto moto2 = new Moto("Yamaha","Factor",2021,"yyy-9999",200,1000);
+//        System.out.println(moto2.exibirDados());
+
         Locadora locadora = new Locadora();
-        locadora.adicionarVeículo(carro1);
-        locadora.adicionarVeículo(moto1);
+        locadora.adicionarVeiculo(carro1);
+        locadora.adicionarVeiculo(moto1);
+        locadora.adicionarVeiculo(carro2);
+        locadora.adicionarVeiculo(moto2);
         locadora.listarVeiculo();
 
+        int tamanho = locadora.getFrota().size();
+        Scanner sc = new Scanner(System.in);
 
+        System.out.printf("Escolha o veiculo (1 a " + tamanho + " ): ");
+        int veiculoSelecionado = sc.nextInt();
+
+        System.out.println("Quantos dias você deseja ficar com o veículo: ");
+        int dias = sc.nextInt();
+
+        locadora.aluguelVeiculo(dias, veiculoSelecionado);
     }
 }
